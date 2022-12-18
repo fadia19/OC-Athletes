97476f55d97f720e6215aeae9a6161d73392eca9
AgoloFadiaTarek <fadia.tarek@agolo.com>
2022-12-18 22:53:40 +0200

    styles adjustment

--- athletes/src/components/AthleteInfo/AthleteInfo.tsx
+++ athletes/src/components/AthleteInfo/AthleteInfo.tsx
@@ -1,4 +1,4 @@
-import { Col, Row } from "antd";
+import { Card, Col, Row } from "antd";
 import { useEffect } from "react";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
@@ -29,59 +29,71 @@ const AthleteInfo = ({
   }, []);
 
   return (
-    <div className="athlete-info-wrapper">
-      <Row>
-        <Col md={{ span: 8 }} sm={{ span: 12 }} xs={{ span: 24 }}>
-          <img
-            src={convertBufferToImage(selectedAthlete.athletePhoto)}
-            alt="athlete"
-            className="athlete-photo"
-          />
-        </Col>
-        <Col md={{ span: 14, offset: 2 }} sm={{ span: 12 }} xs={{ span: 24 }}>
-          <div className="athlete-personal-info">
-            <Row>
-              <Col md={{ span: 12 }} sm={{ span: 24 }} xs={{ span: 24 }}>
-                <p className="info-wrapper">
-                  <span className="field-label">Name: </span>
-                  <span className="field-value">
-                    {selectedAthlete.name} {selectedAthlete.surname}
-                  </span>
-                </p>
-              </Col>
-              <Col md={{ span: 12 }} sm={{ span: 24 }} xs={{ span: 24 }}>
-                <p className="info-wrapper">
-                  <span className="field-label">DOB: </span>
-                  <span className="field-value">
-                    {selectedAthlete.date_of_birth}
-                  </span>
-                </p>
-              </Col>
-            </Row>
+    <>
+      <Card className="athlete-info-card">
+        <div className="athlete-info-wrapper">
+          <Row>
+            <Col md={{ span: 8 }} sm={{ span: 12 }} xs={{ span: 24 }}>
+              <img
+                src={convertBufferToImage(selectedAthlete.athletePhoto)}
+                alt="athlete"
+                className="athlete-photo"
+              />
+            </Col>
+            <Col
+              md={{ span: 14, offset: 2 }}
+              sm={{ span: 12 }}
+              xs={{ span: 24 }}
+            >
+              <div className="athlete-personal-info">
+                <Row>
+                  <Col md={{ span: 12 }} sm={{ span: 24 }} xs={{ span: 24 }}>
+                    <p className="info-wrapper">
+                      <span className="field-label">Name: </span>
+                      <span className="field-value">
+                        {selectedAthlete.name} {selectedAthlete.surname}
+                      </span>
+                    </p>
+                  </Col>
+                  <Col md={{ span: 12 }} sm={{ span: 24 }} xs={{ span: 24 }}>
+                    <p className="info-wrapper">
+                      <span className="field-label">DOB: </span>
+                      <span className="field-value">
+                        {selectedAthlete.date_of_birth}
+                      </span>
+                    </p>
+                  </Col>
+                </Row>
 
-            <Row>
-              <Col md={{ span: 12 }} sm={{ span: 24 }} xs={{ span: 24 }}>
-                <p className="info-wrapper">
-                  <span className="field-label">Weight: </span>
-                  <span className="field-value">{selectedAthlete.weight}</span>
-                </p>
-              </Col>
-              <Col md={{ span: 12 }} sm={{ span: 24 }} xs={{ span: 24 }}>
-                <p className="info-wrapper">
-                  <span className="field-label">Height: </span>
-                  <span className="field-value">{selectedAthlete.height}</span>
-                </p>
-              </Col>
-            </Row>
-          </div>
-          <AthleteMedals
-            medals={selectedAthleteMedals}
-            isLoading={isFetchingAthleteMedals}
-          />
-        </Col>
-      </Row>
+                <Row>
+                  <Col md={{ span: 12 }} sm={{ span: 24 }} xs={{ span: 24 }}>
+                    <p className="info-wrapper">
+                      <span className="field-label">Weight: </span>
+                      <span className="field-value">
+                        {selectedAthlete.weight}
+                      </span>
+                    </p>
+                  </Col>
+                  <Col md={{ span: 12 }} sm={{ span: 24 }} xs={{ span: 24 }}>
+                    <p className="info-wrapper">
+                      <span className="field-label">Height: </span>
+                      <span className="field-value">
+                        {selectedAthlete.height}
+                      </span>
+                    </p>
+                  </Col>
+                </Row>
+              </div>
+              <AthleteMedals
+                medals={selectedAthleteMedals}
+                isLoading={isFetchingAthleteMedals}
+              />
+            </Col>
+          </Row>
+        </div>
+      </Card>
       <AthleteBio bio={selectedAthlete.bio} />
-    </div>
+    </>
   );
 };
 
